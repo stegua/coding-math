ab4a1fb60eddc42d679e52f9c8842d29bf1a9b0e
stegua <stefano.gualandi@gmail.com>
2022-10-06 10:09:24 +0200

    New book version

---
+++ _build/jupyter_execute/Chap3_Programmare_grafici.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # 3. Programmare grafici
+
+# In[ ]:
+
+
+
+
---
+++ _build/jupyter_execute/Chap4_Astrarre_tipi_di_dati.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # 4. Astrarre tipi di dati
+
+# In[ ]:
+
+
+
+
